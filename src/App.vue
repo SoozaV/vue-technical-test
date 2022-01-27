f4e188787b37f2a3e56ace4f34c8e8bb622e08ce
SoozaV <s8za.92@gmail.com>
2022-01-27 03:46:28 -0600

    Vue technical test

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-row class="pb-5">
+        <b-col>
+          <b-button variant="info" v-on:click="imprimir(users)"
+            >Imprimir Usuarios en Consola</b-button
+          >
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col lg="4" md="6" sm="12" v-for="user in users" :key="user.id">
+          <User v-bind:userData="user" />
+        </b-col>
+      </b-row>
+      <b-row class="pb-5">
+        <b-col>
+          <b-button variant="info" v-on:click="imprimir(users)"
+            >Imprimir Usuarios en Consola</b-button
+          >
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import User from "./components/User.vue";
+import axios from "axios";
+
+const baseUrl = "https://reqres.in/api/users?page=2";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    User,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    axios.get(baseUrl).then(({ data }) => {
+      this.users = data.data;
+    });
+  },
+  methods: {
+    imprimir(users) {
+        console.table(JSON.parse(JSON.stringify(users)));
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +63,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+::-moz-selection {
+  /* Code for Firefox */
+  color: rgb(255, 255, 255);
+  background: rgb(145, 145, 145);
+}
+
+::selection {
+  color: rgb(255, 255, 255);
+  background: rgb(145, 145, 145);
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-card
+    img-src="https://picsum.photos/1000/400?grayscale"
+    class="card-profile mb-5"
+  >
+    <div class="card-profile-image">
+      <b-img v-bind:src="userData.avatar" rounded="circle" thumbnail />
+    </div>
+    <b-card-body class="pt-2 pb-4 pl-4 pr-4">
+      <div class="text-center">
+        <span>User ID: {{ userData.id }}</span>
+        <b-form-input @focus="$event.target.select()" class="custom-input" v-model="userData.first_name" placeholder="Enter your first name"></b-form-input>
+        <b-form-input @focus="$event.target.select()" class="custom-input" v-model="userData.last_name" placeholder="Enter your last name"></b-form-input>
+        <b-form-input @focus="$event.target.select()" class="custom-input" v-model="userData.email" placeholder="Enter your email"></b-form-input>
+      </div>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: {
+    userData: Object,
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.card-profile {
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.5rem;
+  background: #ebebeb;
+  box-shadow: none;
+  transition: all 0.2s ease;
+}
+.card-profile:hover {
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.card-profile .card-profile-image img {
+  min-height: 138px;
+  max-width: 138px;
+  min-height: 138px;
+  max-width: 138px;
+  object-fit: cover;
+  transform: scale(1);
+  transition: all 0.2s ease;
+}
+.card-profile:hover .card-profile-image img {
+  transform: scale(0.9);
+}
+.card-profile img {
+  max-width: 100%;
+  border-radius: 0.5rem 0.5rem 0 0;
+}
+.card-profile-image {
+  margin-top: -69px;
+}
+.custom-input {
+  background: none;
+  border: none;
+  text-align: center;
+}
+.custom-input:hover {
+  background: rgba(192, 192, 192, 0.2);
+}
+.custom-input:focus {
+  background: white;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
+</style>
